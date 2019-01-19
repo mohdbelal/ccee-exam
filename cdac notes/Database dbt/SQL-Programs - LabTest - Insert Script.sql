5b09f7f26a152de2b1efe2fd328cc6878207ad84
mohdbelal <32792410+mohdbelal@users.noreply.github.com>
2019-01-19 13:16:45 +0530

    Add files via upload

---
+++ cdac notes/Database dbt/SQL-Programs - LabTest - Insert Script.sql
@@ -0,0 +1,12 @@
+insert statement to use for Student and Course tables.
+
+Insert into Student values (1,'Rohit',18,'Mumbai');
+Insert into Student values (2,'Neha',17,'Pune');
+Insert into Student values (3,'Max,19,’Delhi');
+Insert into Student values (4,'Scott',20,'Goa');
+Insert into Student values (5,'Akash',18,'Mumbai');
+
+Insert into Course values (101,'Networking',2);
+Insert into Course values (102,'Programming',1);
+Insert into Course values (103,'Marketing',5);
+Insert into Course values (104,'Finance',3);
